81a4f050e21c35fd307f36b088efce33328cc8d8
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2019-11-26 12:46:09 +0000

    Add the real regions strategy
    Provide a way to test against only existing regions.
    
    And then watch this list get out of sync

--- hypothesis_aws.py
+++ hypothesis_aws.py
@@ -1,6 +1,35 @@
-from hypothesis.strategies import from_regex
+from hypothesis.strategies import from_regex, sampled_from
 import re
 
+
+def regions():
+
+    real_regions = [
+        "ap-east-1",
+        "ap-northeast-1",
+        "ap-northeast-2",
+        "ap-south-1",
+        "ap-southeast-1",
+        "ap-southeast-2",
+        "ca-central-1",
+        "cn-north-1",
+        "cn-northwest-1",
+        "eu-central-1",
+        "eu-north-1",
+        "eu-west-1",
+        "eu-west-2",
+        "eu-west-3",
+        "me-south-1",
+        "sa-east-1",
+        "us-east-1",
+        "us-east-2",
+        "us-gov-east-1",
+        "us-gov-west-1",
+        "us-west-1",
+        "us-west-2",
+    ]
+
+    return sampled_from(real_regions)
 
 def fake_regions():
     """Generate fake regions that match the AWS region naming format"""
